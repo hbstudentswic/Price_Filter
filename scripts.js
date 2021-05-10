b0361bd46e31aa45a4d8786f4ea629381eb22a37
Hunter Bossetto <hbstudentswic@gmail.com>
2021-05-10 12:38:56 -0500

    Class Updates

--- scripts.js
+++ scripts.js
@@ -1,90 +1,84 @@
 const catalog = [
   {
+    category: 'Sporting Goods',
     price: '$49.99',
+    stocked: true,
     name: 'Football',
-    stocked: true,
   },
   {
+    category: 'Sporting Goods',
     price: '$9.99',
+    stocked: true,
     name: 'Baseball',
-    stocked: true,
   },
   {
+    category: 'Sporting Goods',
     price: '$29.99',
+    stocked: false,
     name: 'Basketball',
-    stocked: false,
   },
   {
+    category: 'Electronics',
     price: '$99.99',
+    stocked: true,
     name: 'iPod Touch',
-    stocked: true,
   },
   {
+    category: 'Electronics',
     price: '$399.99',
-    name: 'iPhone 5',
     stocked: false,
+    name: 'iPhone 5',
   },
-  {
-    price: '$199.99',
-    name: 'Nexus 7',
-    stocked: true,
-  },
+  { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' },
 ];
 
-// Variable Declaration
+// TODO (JS):
+// 1. Grab the tbody.
+const tbody = document.querySelector('tbody'); // Only needs to be grabbed one time
+// 2. Grab the template.
+const template = document.querySelector('template'); // Only needs to be grabbed one time
+
+// 3. Clone the template
+
+// 4. Select all of the 'td''s in the template-->
 
 const ul = document.createElement('ul');
-const range = document.getElementById('price-range');
-const searchitem = document.querySelector('#search-name');
-const stockitem = document.querySelector('#stocked-checkbox');
 
-// Render Function
-function renderItems(maxPrice) {
+/**
+ * Render based on the value of `maxPrice`
+ * @param {number} maxPrice // This is just a comment
+ */
+function render(maxPrice) {
   const filteredCatalog = maxPrice
     ? catalog.filter(({ price }) => price.slice(1) < maxPrice)
-    : catalog;
+    : // If there is no 'maxPriceCatalog' , it will be the same as the catalog array (This is just turnery)
+      catalog;
 
-  ul.innerHTML = filteredCatalog
-    .map(({ name, price }) => `<li>${name} - ${price}</li>`)
-    .join('');
+  // This one is destructring the price property and removing the dollar sign $ in front of it to basicaly make it a number then it is comparing to the maxPrice.
+
+  // For each (or map) item in the 'filteredCatalog' array, clone the template contents (the 'tr')
+
+  // Grab the empty 'td''s
+
+  // Insert 'name' as the 'textContent' in the first, and do the same for 'price' in the second
+
+  // Append the row to the 'tbody'
+
+  filteredCatalog.forEach(product => {
+    const newRow = template.content.cloneNode(true); // The 'content of the template is the stuff inside the 'tr' cloneNode makes a copy of the content.
+    // console.log(newRow); FOR TESTING ONLY: we saw that it made a fragment for every object in the array
+    const tds = newRow.querySelectorAll('td');
+    tds[0].innerText = product.name;
+    tds[1].innerText = product.price;
+
+    tbody.appendChild(newRow);
+  });
 }
 
-// Search Filter
-function filterProductsBySearch(products, search) {
-  const searchedItems = products.filter(({ name }) =>
-    name.toLowerCase().includes(search),
-  );
-  return searchedItems;
-}
+render();
 
-// Stock Filter
-function filterByStocked(products) {
-  const stockedItems = products.filter(({ stocked }) => stocked === true);
-  return stockedItems;
-}
-
-const ItemsStocked = filterByStocked(catalog);
-
-document.body.appendChild(ul);
-renderItems(catalog);
-
-// Slider Event Handler
-range.addEventListener('input', ({ target: { value } }) => {
-  renderItems(Number(value));
-});
-
-// Search Text Box Event Handler
-searchitem.addEventListener('keyup', event => {
-  const letters = event.target.value;
-  const filteredItems = filterProductsBySearch(catalog, letters);
-  renderItems(filteredItems);
-});
-
-// Stocked Items Check Box Event Handler
-stockitem.addEventListener('change', ({ target: { checked } }) => {
-  if (stockitem.checked) {
-    renderItems(ItemsStocked);
-  } else {
-    renderItems(catalog);
-  }
-});
+document
+  .querySelector('input')
+  .addEventListener('input', ({ target: { value } }) => {
+    render(Number(value));
+  });
